0d2a8e36b880479a05982077001c86d531d95546
Hugo Hromic <hhromic@gmail.com>
2014-05-24 02:17:54 +0100

    Made all functions as prototypes.

--- canvasleds.js
+++ canvasleds.js
@@ -8,9 +8,22 @@
 (function () {
     'use strict';
 
+    // Constructor (provide Uint32Array leds for better performance)
+    function CanvasLeds(canvas, leds) {
+        this._canvas = {
+            ledWidth: canvas.getAttribute('width') / leds.length,
+            ledHeight: canvas.getAttribute('height') / 1,
+            ctx: canvas.getContext('2d')
+        };
+        this._leds = leds;
+    }
+
+    // Prototype shortcut
+    var proto = CanvasLeds.prototype;
+
     // Convert HSV (8-bits) to RGB
     // Taken (& adapted) from: http://stackoverflow.com/a/17243070
-    function _hsv8ToRgb (hsv8) {
+    proto._hsv8ToRgb = function (hsv8) {
         var h, s, v, r, g, b, i, f, p, q, t;
         h = hsv8[0] / 0xFF, s = hsv8[1] / 0xFF, v = hsv8[2] / 0xFF;
         i = Math.floor(h * 6);
@@ -29,25 +42,12 @@
         return [Math.floor(r * 0xFF), Math.floor(g * 0xFF), Math.floor(b * 0xFF)];
     }
 
-    // Constructor (provide Uint32Array leds for better performance)
-    function CanvasLeds(canvas, leds) {
-        this._canvas = {
-            ledWidth: canvas.getAttribute('width') / leds.length,
-            ledHeight: canvas.getAttribute('height') / 1,
-            ctx: canvas.getContext('2d')
-        };
-        this._leds = leds;
-    }
-
-    // Prototype shortcut
-    var proto = CanvasLeds.prototype;
-
     // Paint LEDs state into the canvas
     proto.show = function () {
         for (var i=this._leds.length; i--;) {
             var led = this._leds[i];
             if (((led >> 24) & 0xFF) == 0x00) { // led should be painted?
-                var rgb = _hsv8ToRgb([(led >> 16) & 0xFF, (led >> 8) & 0xFF, led & 0xFF]);
+                var rgb = this._hsv8ToRgb([(led >> 16) & 0xFF, (led >> 8) & 0xFF, led & 0xFF]);
                 this._canvas.ctx.fillStyle = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
                 this._canvas.ctx.fillRect(this._canvas.ledWidth * i, 0,
                     this._canvas.ledWidth - 1, this._canvas.ledHeight);
