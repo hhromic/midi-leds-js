c53b226ad7e6d625c57f731ed43ba0446adc9b1c
Hugo Hromic <hhromic@gmail.com>
2015-01-20 01:03:27 +0000

    Normalised function parameter names

--- cchelper.js
+++ cchelper.js
@@ -48,44 +48,43 @@
     /**
      * Processes a MIDI Control Change (CC) message.
      *
-     * @param {number} _channel - the channel of the CC message.
-     * @param {number} _control - the control number of the CC message.
-     * @param {number} _value - the control value of the CC message.
+     * @param {number} channel - the channel of the CC message.
+     * @param {number} control - the control number of the CC message.
+     * @param {number} value - the control value of the CC message.
      */
-    proto.controlChange = function (_channel, _control, _value) {
-        var channel = _channel & 0xF;
-        var control = _control & 0x7F;
-        var value = _value & 0x7F;
-        switch (control) {
+    proto.controlChange = function (channel, control, value) {
+        var _channel = channel & 0xF;
+        var _value = value & 0x7F;
+        switch (control & 0x7F) {
             case 0x14: // Change Palette
-                this._midiColors.setPalette(channel, value);
+                this._midiColors.setPalette(_channel, _value);
                 return;
             case 0x15: // Change Color Map
-                this._midiColors.setColorMap(channel, value);
+                this._midiColors.setColorMap(_channel, _value);
                 return;
             case 0x16: // Change Fixed Hue
-                this._midiColors.setFixedHue(channel, Math.round(0xFF * (value / 0x7F)));
+                this._midiColors.setFixedHue(_channel, Math.round(0xFF * (_value / 0x7F)));
                 return;
             case 0x17: // Change Attack Time
-                this._midiLeds.setAttackTime(channel, Math.round(this._timeRange * (value / 0x7F)));
+                this._midiLeds.setAttackTime(_channel, Math.round(this._timeRange * (_value / 0x7F)));
                 return;
             case 0x18: // Change Decay Time
-                this._midiLeds.setDecayTime(channel, Math.round(this._timeRange * (value / 0x7F)));
+                this._midiLeds.setDecayTime(_channel, Math.round(this._timeRange * (_value / 0x7F)));
                 return;
             case 0x19: // Change Sustain Level
-                this._midiLeds.setSustainLevel(channel, 1.0 * (value / 0x7F));
+                this._midiLeds.setSustainLevel(_channel, 1.0 * (_value / 0x7F));
                 return;
             case 0x1A: // Change Release Time
-                this._midiLeds.setReleaseTime(channel, Math.round(this._timeRange * (value / 0x7F)));
+                this._midiLeds.setReleaseTime(_channel, Math.round(this._timeRange * (_value / 0x7F)));
                 return;
             case 0x1B: // Change Ignore Velocity
-                this._midiColors.setIgnoreVelocity(channel, value < 0x40 ? false : true);
+                this._midiColors.setIgnoreVelocity(_channel, _value < 0x40 ? false : true);
                 return;
             case 0x1C: // Change Base Brightness
-                this._midiLeds.setBaseBrightness(channel, value);
+                this._midiLeds.setBaseBrightness(_channel, _value);
                 return;
             case 0x1D: // Change Enabled State
-                this._midiLeds.setEnabled(channel, value < 0x40 ? false : true);
+                this._midiLeds.setEnabled(_channel, _value < 0x40 ? false : true);
                 return;
         }
     }
